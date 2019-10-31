7a03f4fd1b1ced4d7672b2f405c512e42f18789e
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2019-10-31 16:58:42 -0300

    feature: Created password encrypt test

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,11 +1,25 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
+  });
+
+  it('should encrypt user password when new user are created', async () => {
+    const user = await User.create({
+      name: 'Luiz Andre',
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should be able to register', async () => {
