906b4720c240906ddf4fd08d2eb7ca07cce05861
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2019-10-31 16:41:14 -0300

    feature: Double email test created and failed

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -13,4 +13,24 @@ describe('User', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('shoult not be alble to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Luiz Andre',
+        email: 'teste@teste.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Luiz Andre',
+        email: 'teste@teste.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
+  });
 });
